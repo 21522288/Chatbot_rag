d57daca2a022d2e75912012d2b6b3f0721901505
21522288 <116091982+21522288@users.noreply.github.com>
2024-11-03 16:27:35 +0700

    build project langchain, chromadb, choose embedding model

---
+++ Chatbot_rag/get_embedding_function.py
@@ -0,0 +1,13 @@
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+# from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+
+
+def get_embedding_function():
+   # embeddings = HuggingFaceInferenceAPIEmbeddings(
+   #  api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN"), model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+   # )
+   embeddings = FastEmbedEmbeddings()
+   return embeddings
